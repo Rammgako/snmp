09369e1164629b0aa4cf868a2e85d22ce7bfb5c3
Kai <33143466+Rammgako@users.noreply.github.com>
2017-11-24 16:11:45 +0900

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,16 @@
 class snmp (
     String $package_name                     = $snmp::params::package_name,
-    String $service_name                     = $snmp::params::service_name
+    String $trapsink                         = $snmp::params::trapsink, #for snnmpd.conf
+    String $sec_name                         = $snmp::params::sec_name, #for snnmpd.conf
+    String $source                           = $snmp::params::source,  #for snnmpd.conf
+    String $community                        = $snmp::params::comunity, #for snnmpd.conf
+    String $sec_group                        = $snmp::params::sec_group, #for snnmpd.conf
+    String $sec_model                        = $snmp::params::sec_model,#for snnmpd.conf
+    Array[String] $snmpagent                 = $snmp::params::snmpagent,
     Array[String] $service_name              = $snmp::params::service_name,
     Array[String] $additional_packages       = $snmp::params::additional_packages,
-    Array[String] $mibs_dir                  = $snmp::params::mibs_dir,
-    Array[String] $mibs_file                 = $snmp::params::mibs_file,
+    Array[String] $mibs_dirs                 = $snmp::params::mibs_dirs,
+    Array[String] $mibs_files                = $snmp::params::mibs_files,
     Array[String] $mibs                      = $snmp::params::mibs,
     Any $port                                = $snmp::params::port,
     Any $version                             = $snmp::params::version,
