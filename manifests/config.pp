ea1e3bd5cb9c39ccf27b6ef8b67e9c767a57ab54
Kai <33143466+Rammgako@users.noreply.github.com>
2017-11-24 16:39:39 +0900

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,30 +5,10 @@ class snmp::snmp_config (
     $mibs                  = $snmp::mibs,
     $mib_errors            = $snmp::mib_errors,
     $log_timestamp         = $snmp::log_timestamp,
-    $extra_config_sections = 
+    $extra_config_sections = $snmp::extra_config_sections,
 )   inherits snmp {
-
-    concat{$config:
-        ensure => present,
-        owner => 'root',
-        group => 'root',
-        mode => 0640
-    }
         
-    concat::fragment{'snmp_header':
-        target  => $config,
-        content => template('snmp/snmp.header.conf.erb'),
-        order   => '01',
-  }
-  
-    if $mibs_files {
-        create_resources('snmp::mibs_file, $mibs_files')
-    }
-    
-    if $mibs_dirs {
-        create_resources('snmp::mibs_dir, $mibs_dirs')
-    }
-    
-    if $mibs {
-        create_resources('snmp::mib, $mibs')
-    }
+        file {'/etc/snmp/snmp.conf':
+            ensure => present,
+            content => template('snmp/snmp.header.conf.erb')
+        }
